39b382c46eb1c4db7725a8029e5fc3573d45bf20
Elizabeth Woodard <eswoodard75@gmail.com>
2018-07-19 15:35:46 -0400

    initial test added

---
+++ test/test-petfolio-integration.js
@@ -0,0 +1,16 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const express = require('express');
+const app = express();
+chai.use(chaiHttp);
+
+it('returned status 200 and type html', function() {
+  chai.request('http://localhost:8080')
+    .get('/')
+    .end(function (req, res) {
+      expect(res).to.have.status(200);
+      expect(res).to.have.header('content-type', 'text/html');
+      done();
+    });
+  });
+
