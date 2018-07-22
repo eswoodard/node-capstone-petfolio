f84d40060f5fdb6f490d4246fa7a2d46fa5fc6a6
Elizabeth Woodard <eswoodard75@gmail.com>
2018-07-21 20:00:25 -0400

    added sign in and create account form

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "standard",
+
+    "rules": {
+        "func-names": 0,
+        "prefer-destructuring": 0,
+        "no-console": 0,
+        "no-undef": 0,
+    }
+
+};
--- public/app.js
+++ public/app.js
@@ -1,16 +1,85 @@
-console.log(STORE);
+console.log(STORE)
 
-function renderPetList() {
-  const listHtml=STORE.pets.map(pet => {
-    return `
-    <li>
-      <p>Name: ${pet.name}</p>
-      <img src="${pet.media[0]}"/>
-    </li>`
-  }).join('');
-  console.log(listHtml);
-  const petList = document.querySelector('.pet-list');
-  petList.innerHTML = listHtml;
+function renderAppInfo () {
+  $('.app-body').html(`
+  <div class="app-info">
+    <p class="app-info-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet tellus cras adipiscing. Lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet. Dictum non consectetur a erat nam. Dictum at tempor commodo ullamcorper a lacus vestibulum sed.</p>
+  </div>
+  <div class="account-btns">
+    <button class="sign-in-btn button" type="submit">Sign In</button>
+    <p>Don't have an account?  Create one now:</p>
+    <button class="create-account-btn">Create Account</button>
+  </div>
+  `
+  )
 }
 
-renderPetList();
+function renderSignInForm () {
+  return `
+   <div class="sign-in-form">
+    <h2>Sign In<h2>
+    <form action="/server.js">
+      <label for="username">Username</label>
+      <input type="text" placeholder="name@domain.com" name="username" required></br>
+      <label for="password">Password</label>
+      <input type="password" placeholder="Enter Password" name="password" required></br>
+      <button type="submit">Login</button>
+    </form>
+  </div>`
+}
+
+function displaySignInForm () {
+  $('.sign-in-btn').on('click', event => {
+    event.preventDefault()
+    $('.app-body').html(renderSignInForm)
+  })
+}
+
+function renderCreateAccountForm () {
+  return `
+  <div class="create-account-form">
+    <h2>Create Account</h2>
+    <form action="/server.js">
+      <label for="firstName">First Name</label>
+      <input type="text" placeholder="Jane" name="firstName" required></br>
+      <label for="lastName">Last Name</label>
+      <input type="text" placeholder="Doe"  name="lastName" required></br>
+      <label for="username">Username</label>
+      <input type="text" placeholder="name@domain.com" name="username" required></br>
+      <label for="password">Password</label>
+      <input type="password" placeholder="Enter Password" name="password" required></br>
+      <label for="confirm-password">Confirm Password</label>
+      <input type="password" placeholder="Re-enter Password" name="confirm-password" required></br>
+      <button type="submit">Create Account</button>
+   </form>
+ </div>`
+}
+
+function displayCreateAccountForm () {
+  $('.create-account-btn').on('click', event => {
+    event.preventDefault()
+    $('.app-body').html(renderCreateAccountForm)
+  })
+}
+
+// function renderPetList() {
+//   const listHtml=STORE.pets.map(pet => {
+//     return `
+//     <li>
+//       <p>Name: ${pet.name}</p>
+//       <img src="${pet.media[0]}"/>
+//     </li>`
+//   }).join('');
+//   console.log(listHtml);
+//   const petList = document.querySelector('.pet-list');
+//   petList.innerHTML = listHtml;
+// }
+
+function handleAppLoad () {
+  renderAppInfo()
+  displaySignInForm()
+  displayCreateAccountForm()
+  // renderPetList();
+}
+
+$(handleAppLoad)
